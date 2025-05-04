30525a0e14c520fee2b81f728778cb07164a4424
workhub101 <exploresub0@gmail.com>
2025-05-04 10:48:14 -0400

    Added Javascipt

---
+++ calculator.js
@@ -0,0 +1,20 @@
+
+function append(value){
+    document.getElementById("display").value += value
+}
+function calculate(){
+    try{
+        document.getElementById("display").value = eval(document.getElementById("display").value)
+    }
+    catch{
+        document.getElementById("display")
+        alert("error")
+    }
+}
+function clearDisplay(){
+    document.getElementById("display").value = " "
+    
+}
+function deleteLastValue(){
+    document.getElementById("display").value = document.getElementById("display").value.slice(0,-1)
+}
